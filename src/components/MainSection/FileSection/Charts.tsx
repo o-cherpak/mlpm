6f07cc0a0379ee31e30b1f5c254d8080e78b132d
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-08-02 14:36:37 +0200

    add charts, add select buttons

--- src/components/MainSection/FileSection/Charts.tsx
+++ src/components/MainSection/FileSection/Charts.tsx
@@ -1,13 +1,66 @@
+import {CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis} from "recharts";
 import type {ExperimentGroups} from "../../../interfaces/GroupedDataInterface.ts";
+import type {ExperimentLog} from "../../../interfaces/FileInterface.ts";
 
-type ChartProps = {
-  data: ExperimentGroups
-}
+type ChartsProps = {
+  groupedData: ExperimentGroups;
+  selectedMetrics: string;
+  selectedExperiments: string[];
+};
 
-export function Charts({data}: Readonly<ChartProps>) {
+type MergedPoint = {
+  step: number;
+  [experimentId: string]: number | null;
+};
+
+export function Charts({groupedData, selectedMetrics, selectedExperiments}: ChartsProps) {
+  const baseData = groupedData[selectedExperiments[0]]?.[selectedMetrics];
+
+  if (!baseData || baseData.length === 0) return null;
+  if (selectedExperiments.length === 0) return null;
+
+  console.log("baseData", baseData);
+
+
+  const stepSize = 10;
+
+  const sampledData: ExperimentLog[] = [];
+  for (let i = 0; i < baseData.length; i += stepSize) {
+    sampledData.push(baseData[i]);
+  }
+
+  const mergedData: MergedPoint[] = sampledData.map(point => {
+    const item: MergedPoint = {step: point.step};
+
+    selectedExperiments.forEach(expId => {
+      const experimentPoints = groupedData[expId]?.[selectedMetrics];
+      const found = experimentPoints.find(p => p.step === point.step);
+      item[expId] = found ? found.value : null;
+    });
+
+    return item;
+  });
+
+  console.log("mergedData", mergedData);
+
   return (
-    <div>
-
-    </div>
-  )
+    <LineChart width={720} height={480} data={mergedData}>
+      <CartesianGrid strokeDasharray="3 3"/>
+      <XAxis dataKey="step"/>
+      <YAxis/>
+      <Tooltip/>
+      {selectedExperiments.map((experimentId, index) => (
+        <Line
+          key={experimentId}
+          type="monotone"
+          dataKey={experimentId}
+          name={`Experiment ${experimentId}`}
+          stroke={["#8884d8", "#82ca9d", "#ff7300"][index % 3]}
+          dot={false}
+          isAnimationActive={false}
+          connectNulls={true}
+        />
+      ))}
+    </LineChart>
+  );
 }
--- src/components/MainSection/FileSection/FileReader.tsx
+++ src/components/MainSection/FileSection/FileReader.tsx
@@ -1,40 +1,84 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Papa from "papaparse";
-import type {ExperimentLog} from "../../../interfaces/FileInterface.ts";
-import type {ExperimentGroups} from "../../../interfaces/GroupedDataInterface.ts";
-import {groupData} from "../../../services/GroupData.ts";
+import type { ExperimentLog } from "../../../interfaces/FileInterface.ts";
+import type { ExperimentGroups } from "../../../interfaces/GroupedDataInterface.ts";
+import { groupData } from "../../../services/GroupData.ts";
+import { Charts } from "./Charts.tsx";
 
 type FileReaderProps = {
   file: File;
 };
 
-export function FileReader({file}: Readonly<FileReaderProps>) {
-  const [data, setData] = useState<ExperimentLog[]>([]);
+export function FileReader({ file }: Readonly<FileReaderProps>) {
   const [groupedData, setGroupedData] = useState<ExperimentGroups>({});
-
+  const [selectedExperiments, setSelectedExperiments] = useState<string[]>([]);
+  const [selectedMetric, setSelectedMetric] = useState<string>("");
 
   useEffect(() => {
-    if (!file) {
-      setData([]);
-      return;
-    }
-
     Papa.parse<ExperimentLog>(file, {
       header: true,
       skipEmptyLines: true,
       complete: (results) => {
-        setData(results.data);
+        setGroupedData(groupData(results.data));
       },
     });
   }, [file]);
 
-  useEffect(() => {
-    setGroupedData(groupData(data));
-  }, [data]);
+  const handleExperimentToggle = (key: string) => {
+    setSelectedExperiments((prev) =>
+      prev.includes(key)
+        ? prev.filter((item) => item !== key)
+        : [...prev, key]
+    );
+  };
+
+  const handleMetricSelect = (key: string) => {
+    setSelectedMetric(key);
+  };
 
   return (
-    <div>
+    <div className="flex pt-10 gap-20">
+      <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-2">
+          {Object.keys(groupedData).map((key) => (
+            <label
+              key={key}
+              className={`cursor-pointer border rounded-xl px-4 py-2 transition ${
+                selectedExperiments.includes(key)
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-100 hover:bg-gray-200"
+              }`}
+              onClick={() => handleExperimentToggle(key)}
+            >
+              {key}
+            </label>
+          ))}
+        </div>
 
+        <div className="flex flex-col gap-2">
+          {Object.keys(groupedData).length > 0 &&
+            Object.keys(groupedData["exp_1"]).map((key) => (
+              <label
+                key={key}
+                className={`cursor-pointer border rounded-xl px-4 py-2 transition ${
+                  selectedMetric === key
+                    ? "bg-green-500 text-white"
+                    : "bg-gray-100 hover:bg-gray-200"
+                }`}
+                onClick={() => handleMetricSelect(key)}
+              >
+                {key}
+              </label>
+            ))}
+        </div>
+      </div>
+
+
+      <Charts
+        groupedData={groupedData}
+        selectedExperiments={selectedExperiments}
+        selectedMetrics={selectedMetric}
+      />
     </div>
   );
 }
