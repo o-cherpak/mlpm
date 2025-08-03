f553af1483f2f2348c75c6d6385d9970101584de
Oleksandr Cherpak <sashacherpak2005@gmail.com>
2025-08-03 14:07:40 +0200

    add "after" and hover effect on nav in Navigation.tsx

--- src/components/Header/Navigation.tsx
+++ src/components/Header/Navigation.tsx
@@ -13,10 +13,15 @@ export function Navigation() {
         <a
           key={link.href}
           href={link.href}
-          className={"bg-gradient-to-r from-blue-700 via-purple-600 to-blue-700 bg-clip-text text-transparent"}
+          className="
+          relative bg-gradient-to-r from-blue-700 via-purple-600 to-blue-700 bg-clip-text text-transparent
+          after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0
+          after:bg-gradient-to-r after:from-blue-600 after:to-pink-600 after:to-pink-400
+          after:transition-all after:duration-400 hover:after:w-full"
         >
-            {link.label}
+          {link.label}
         </a>
+
       ))}
     </nav>
   )
